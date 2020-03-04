61647ca37f94acfd0e61fa951a0ad201e10c4921
Soliman Refaat <incredible.soliman@yahoo.com>
2020-03-04 23:51:54 +0200

    upload project

---
+++ app/src/main/java/com/soli/simpleretrofitlistview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.soli.simpleretrofitlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
